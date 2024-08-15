4498444387d210cbf0083c2bcfd5891e0ef0682e
WIsdom Dauda <codewithise@gmail.com>
2024-08-15 16:55:39 +0100

    5th

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let imgs = [
+  "asset/IMG-20240813-WA0009.jpg",
+  "asset/IMG-20240813-WA0008.jpg",
   "asset/bread.jpg",
   "asset/download.jpeg",
   "asset/hh.webp",
   "asset/hhdfsd.webp",
-  "asset/IMG-20240813-WA0008.jpg",
-  "asset/IMG-20240813-WA0009.jpg",
   "asset/vaselin.jpg",
   //   "asset/ICON.png",
 ];
@@ -12,7 +12,7 @@ let index = 0;
 
 let img = document.getElementById("img");
 
-setInterval(changeImg, 4000);
+setInterval(changeImg, 5000);
 function changeImg() {
   if (index < 5) {
     index++;
